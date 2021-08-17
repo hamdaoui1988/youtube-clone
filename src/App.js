c374963417021fed8cb654181f6a8457c6ab543a
hamdaoui1988 <hamdaoui.noureddine@gmail.com>
2021-08-18 07:10:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+import './Sidebar.css';
+import './RecommendedVideos.css';
+import SearchPage from './SearchPage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+y
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+      <Header />
+        <Switch>
+          <Route path="/search/:searchTerm">
+          
+          <div className="App-page">
+                   <Sidebar className="sidebar" />
+                   <SearchPage />
+                   </div>
+          </Route>
+          <Route path="/">
+               
+                   <div className="App-page">
+                   <Sidebar className="sidebar" />
+                   <RecommendedVideos className="recommendedvideos" />
+                   </div>
+          </Route>
+        </Switch>
+      </Router>
+      
+
     </div>
+    
   );
 }
 
